355ed1ca1f92963bbb84857cf3a6c3276757be12
Haithem Araissia <haithemaraissia@gmail.com>
2013-04-15 22:26:34 -0500

    Coming back and trying to get
    paging and sorting working

--- RentalMVC2010/RentalMVC2010/Controllers/UnitGalleryController.cs
+++ RentalMVC2010/RentalMVC2010/Controllers/UnitGalleryController.cs
@@ -6,9 +6,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using RentalMVC2010.Models;
+using PagedList;
 
 namespace RentalMVC2010.Controllers
-{ 
+{
     public class UnitGalleryController : Controller
     {
         private DB_33736_rentalEntities db = new DB_33736_rentalEntities();
@@ -16,10 +17,46 @@ namespace RentalMVC2010.Controllers
         //
         // GET: /UnitGallery/
 
-        public ViewResult Index()
+        public ViewResult Index(int? page, string sortBy)
         {
-            var unitgalleries = db.UnitGalleries.Include(u => u.Unit);
-            return View(unitgalleries.ToList());
+            ViewBag.UnitSort = String.IsNullOrEmpty(sortBy) ? "UnitGalleryId desc" : "";
+            ViewBag.GallerySort = sortBy == "UnitId" ? "UnitId desc" : "UnitId";
+
+            var unitgalleries = db.UnitGalleries.Include(u => u.Unit).OrderBy(u => u.UnitGalleryId);
+
+            switch (sortBy)
+            {
+                case "UnitGalleryId desc":
+                    unitgalleries = unitgalleries.OrderByDescending(s => s.UnitGalleryId);
+                    break;
+
+                case "UnitGalleryId":
+                    unitgalleries = unitgalleries.OrderBy(s => s.UnitGalleryId);
+                    break;
+
+                case "UnitId desc":
+                    unitgalleries = unitgalleries.OrderByDescending(s => s.UnitId);
+                    break;
+
+                case "UnitId c":
+                    unitgalleries = unitgalleries.OrderBy(s => s.UnitId);
+                    break;
+
+
+                default:
+                    unitgalleries = unitgalleries.OrderByDescending(s => s.UnitGalleryId);
+                    break;
+            }
+
+
+
+            if (Request.HttpMethod != "GET")
+            {
+                page = 1;
+            }
+            int pageSize = 2;
+            int pageNumber = (page ?? 1);
+            return View(unitgalleries.ToPagedList(pageNumber, pageSize));
         }
 
         //
@@ -38,7 +75,7 @@ namespace RentalMVC2010.Controllers
         {
             ViewBag.UnitId = new SelectList(db.Units, "UnitId", "Address");
             return View();
-        } 
+        }
 
         //
         // POST: /UnitGallery/Create
@@ -50,16 +87,16 @@ namespace RentalMVC2010.Controllers
             {
                 db.UnitGalleries.Add(unitgallery);
                 db.SaveChanges();
-                return RedirectToAction("Index");  
+                return RedirectToAction("Index");
             }
 
             ViewBag.UnitId = new SelectList(db.Units, "UnitId", "Address", unitgallery.UnitId);
             return View(unitgallery);
         }
-        
+
         //
         // GET: /UnitGallery/Edit/5
- 
+
         public ActionResult Edit(int id)
         {
             UnitGallery unitgallery = db.UnitGalleries.Find(id);
@@ -85,7 +122,7 @@ namespace RentalMVC2010.Controllers
 
         //
         // GET: /UnitGallery/Delete/5
- 
+
         public ActionResult Delete(int id)
         {
             UnitGallery unitgallery = db.UnitGalleries.Find(id);
@@ -97,7 +134,7 @@ namespace RentalMVC2010.Controllers
 
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
-        {            
+        {
             UnitGallery unitgallery = db.UnitGalleries.Find(id);
             db.UnitGalleries.Remove(unitgallery);
             db.SaveChanges();
